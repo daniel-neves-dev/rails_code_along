6c298e7e97d4af28126b189e9243e3feb1ed3080
daniel-dev-neves <daniel.oliveira.au@hotmail.com>
2024-03-24 11:54:36 -0300

    feature: post approved

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def status_label(status)
+    status_generator status
+  end
+
+  private
+
+  def status_generator(status)
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: 'd-inline p-2 text-bg-primary')
+    when 'approved'
+      content_tag(:span, status.titleize, class: 'd-inline p-2 text-bg-success')
+    when 'rejected'
+      content_tag(:span, status.titleize, class: 'd-inline p-2 text-bg-danger')
+    end
+  end
 end
